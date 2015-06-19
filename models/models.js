e8d0753de676638c4bf8b129a429851940b33ddf
Miguel on XPS <miguel@guidedelearning.com>
2015-06-19 08:13:35 +0100

    Despliegue DB en Heroku

--- models/models.js
+++ models/models.js
@@ -1,14 +1,45 @@
 var path = require('path');
 
+// Definimos un esquema "general" que nos permitira obtener los valores de la variable de entorno DATABASE_URL situada en el fichero .env
+// De este modo no nos tenemos que preoucpar de definir dos esquemas diferentes para local y remoto (Heroku)
+// Cuando el programa se ejecute en Heroku, la variable DATABASE_URL contendra los valores postgres
+// Cuando el programa se ejecute en local, la variable DATABASE_URL contendra la referencia a SQLite
+
+// NOTA: A partir de ahora tenemos que ejecutar el programa mediante foreman start y no mediante node o npm start
+// NOTA2: hay que recordar que tenemos que mover la dependencia sqlite del fichero package.json a "devDependencies"
+// NOTA3: foreman se ejecuta en el puerto 5000
+
+// Postgres DATABASE_URL = postgres://user:passwd@host:port/database
+// SQLite 	DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name = (url[6] || null);
+var user 	= (url[2] || null);
+var pwd 	= (url[3] || null);
+var protocol= (url[1] || null);
+var dialect = (url[1] || null);
+var port 	= (url[5] || null);
+var host 	= (url[4] || null);
+var storage = process.env.DATABASE_STORAGE;
+
 // Cargar Modelo ORM
 var Sequelize = require('sequelize');
 
-// USar BDD SQLite
+// Reemplazamos la definicion de SQLite por nuestro modelo dinamico
+var sequelize = new Sequelize(DB_name, user, pwd, {
+	dialect : protocol,
+	protocol: protocol,
+	port 	: port,
+	host 	: host,
+	storage : storage, 	// solo SQLite (.env)
+	omitNull: true 		// definido solo para Heroku PostgreSQL
+});
+/*
 var sequelize = new Sequelize(null, null, null,
 					{
 						dialect: "sqlite",
 						storage: "quiz.sqlite"
 					});
+*/
 
 // Importar la definicion de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
