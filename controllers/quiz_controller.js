99cfe5d99014f31596ed2c4464e321eaacc34b8e
Miguel on XPS <miguel@guidedelearning.com>
2015-06-24 14:42:49 +0100

    validar y editar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -48,14 +48,17 @@ exports.index = function(req, res, next){
 	}
 
 	models.Quiz.findAll(query).then(function(quizes){
-		res.render('quizes/index', {quizes: quizes});
+		res.render('quizes/index', {
+			quizes: quizes,
+			errors: []
+		});
 	}).catch(function(error){
 		next(error);
 	});
 };
 
 exports.show = function(req, res){
-	res.render('quizes/show', {quiz: req.quiz});
+	res.render('quizes/show', {quiz: req.quiz, errors: []});
 };
 
 exports.answer = function(req, res){
@@ -67,7 +70,8 @@ exports.answer = function(req, res){
 
 	res.render('quizes/answer', {
 		quiz: req.quiz,
-		respuesta: resultado
+		respuesta: resultado,
+		errors: []
 	});
 };
 
@@ -80,14 +84,63 @@ exports.new = function(req, res)
 		}
 	);
 
-	res.render('quizes/new', {quiz: quiz});
+	res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
 exports.create = function(req, res)
 {
 	var quiz = models.Quiz.build(req.body.quiz);
 
-	quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-		res.redirect('/quizes');
+	quiz
+	.validate()
+	.then(function(err){
+		if(err)
+		{
+			res.render('quizes/new', {
+				quiz: quiz,
+				errors: err.errors
+			});
+		}
+		else
+		{
+			quiz
+			.save({fields: ["pregunta", "respuesta"]})
+			.then(function(){
+				res.redirect('/quizes');
+			});
+		}
+	});
+};
+
+exports.edit = function(req, res){
+	var quiz = req.quiz;
+	res.render('quizes/edit', {
+		quiz: quiz,
+		errors: []
+	})
+};
+
+exports.update = function(req, res){
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+
+	req.quiz
+	.validate()
+	.then(function(err){
+		if(err)
+		{
+			res.render('quizes/new', {
+				quiz: req.quiz,
+				errors: err.errors
+			});
+		}
+		else
+		{
+			req.quiz
+			.save({fields: ["pregunta", "respuesta"]})
+			.then(function(){
+				res.redirect('/quizes');
+			});
+		}
 	});
 };
--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,11 @@ var quizController = require('../controllers/quiz_controller.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', errors: [] });
 });
 /* pagina de creditos */
 router.get('/author', function(req, res, next) {
-  res.render('author', { fecha: new Date().getFullYear() });
+  res.render('author', { fecha: new Date().getFullYear(), errors: [] });
 });
 
 // si el router encuentra el parametro :quizId en la URL, ejecuta el autoloader
@@ -18,10 +18,14 @@ router.param('quizId', quizController.load);
 // la primera devuelve la lista de todas las preguntas
 // la segunda permite acceder a una pregunta por id (la expresion regular permite solo numberos en el id (\\d+))
 // la tercera permite comprobar la respuesta en funcion del id de pregunta
-router.get('/quizes', quizController.index);
-router.get('/quizes/:quizId(\\d+)', quizController.show);
-router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
-router.get('/quizes/new', quizController.new);
-router.post('/quizes/create', quizController.create);
+router.get('/quizes', 						quizController.index);
+router.get('/quizes/:quizId(\\d+)', 		quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', 	quizController.answer);
+router.get('/quizes/new', 					quizController.new);
+router.post('/quizes/create', 				quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit', 	quizController.edit);
+router.put('/quizes/:quizId(\\d+)', 		quizController.update);
+// para que se haga el put necesitamos el paquete method-override y anadir
+// el parametro _method=put a nuestro form action
 
 module.exports = router;
