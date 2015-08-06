d159573af97ccec9e053a399705e67f4d662855b
Miguel on XPS <miguel@guidedelearning.com>
2015-08-06 14:25:17 +0100

    anadida implementacion con Promise.all

--- controllers/statistics_controller.js
+++ controllers/statistics_controller.js
@@ -42,3 +42,45 @@ exports.get = function(req, res, next)
 		});
 	});
 };
+
+
+/*
+	Implementacion con Promise.all (queda pendiente de probar)
+
+	var models = require('../models/models.js');
+	var Sequelize = require('sequelize');
+
+	 var statsData = {};
+
+	 exports.obtainData = function(req, res, next) {
+	 //Se ha usado el método all de Promises (implementado ya en sequelize), ya que
+	 //de esta forma se ejecutan las consultas asíncronamente en paralelo y se
+	 //continúa cuando han acabado todas.
+	    Sequelize.Promise.all([
+	        models.Quiz.count(),
+	        models.Comment.count(),
+	//Se ha añadido nuevos métodos al modelo Comment en models/comment.js
+	//Para ello se han seguido las instucciones de la documentación de sequelize:
+	//http://docs.sequelizejs.com/en/latest/docs/models-definition/#expansion-of-models
+	        models.Comment.countDistinctQuizId(),
+	        models.Comment.countPublished()
+	    ]).then( function( values ){
+	        statsData.quizes=values[0];
+	        statsData.comments=values[1];
+	        statsData.commentedQuizes=values[2];
+	        statsData.publishedComments=values[3];
+	    }).catch( function (err) {
+	        next(err);
+	    }).finally( function() {
+	        next();
+	    });
+	};
+
+	// GET /quizes/statistics
+	exports.show = function(req, res) {
+	    res.render('statistics', {
+	        statsData: statsData,
+	        errors: []
+	    });
+	};
+*/
